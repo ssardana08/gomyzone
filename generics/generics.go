ff9d8a54f453c3cec11a9966b4a5b4058b61de71
Sumit <sardana.sumit08@gmail.com>
2022-07-29 17:24:47 +0530

    - generics example added with generic interfaces

--- generics/generics.go
+++ generics/generics.go
@@ -10,10 +10,23 @@ func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
 	return s
 }
 
+type Number interface {
+	int64 | float64
+}
+
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
 func main() {
 	floats := map[string]float64{
 		"first":  35.98,
 		"second": 26.99,
 	}
 	fmt.Println(SumIntsOrFloats(floats))
+	fmt.Println(SumNumbers(floats))
 }
